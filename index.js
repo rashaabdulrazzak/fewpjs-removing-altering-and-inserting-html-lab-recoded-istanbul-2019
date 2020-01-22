068ece291feba6cdcbc36d76c064260f759da7f3
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-01-22 19:50:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Write your code here
 document.querySelector('main#main').remove();
+<<<<<<< HEAD
 let newHeader = document.createElement('h1');
 newHeader.id='victory';
 newHeader.innerHTML = 'RASHA is the champion';
 document.body.appendChild(element);
+=======
+let 
+>>>>>>> be44a54cf3f5c547de8a15eb1e7d19e5a26b4a57
